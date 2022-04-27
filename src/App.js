71568480242e9cb1c19bd5a47b35617ea4f6387a
Mien <2502mien@gmail.com>
2022-04-28 00:44:20 +0700

    add config

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import {
   Link,
 } from "react-router-dom";
 import About from './components/About';
+import Counter  from './pages/Counter';
+import Product from './pages/Product';
 export default function App() {
   return (
     <div>
@@ -14,6 +16,12 @@ export default function App() {
           <Route path="/" element={<About />} >
         
           </Route>
+          <Route path="/product" element={<Product />} >
+        
+          </Route>
+          <Route path="/counter" element={<Counter />} >
+        
+        </Route>
 
         </Routes>
         {/* <Link to={`/about?name=mien&age=20`}>about</Link> */}
